241d8e08f62f24256f4bb6b8e8f615146f75e10c
baarenasg <baarenasg@unal.edu.co>
2023-07-03 19:39:41 -0500

    Update DojoStreamTest.java
    Solución de problemas.

--- src/test/java/com/example/demo/DojoStreamTest.java
+++ src/test/java/com/example/demo/DojoStreamTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class DojoStreamTest {
 
@@ -41,23 +43,63 @@ public class DojoStreamTest {
 
     @Test
     void mejorJugadorConNacionalidadFrancia(){
+
+        Predicate<Player> nacionalidadFrancia = player -> player.getNational().equals("France");
+        Optional<Player> result = CsvUtilFile.getPlayers().stream()
+                .filter(nacionalidadFrancia)
+                .reduce((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2);
+        result.ifPresent(System.out::println);
     }
 
 
     @Test
     void clubsAgrupadosPorNacionalidad(){
+        Map<String, Set<String>> result = CsvUtilFile.getPlayers().stream()
+                .collect(Collectors.groupingBy(
+                        Player::getNational,
+                        HashMap::new,
+                        Collectors.mapping(
+                                Player::getClub,
+                                Collectors.toSet()
+                        )
+                ));
+        result.forEach((key, value) -> {
+            System.out.println(key + ": \n");
+            System.out.println(value);
+            System.out.println("\n");
+        });
     }
 
     @Test
     void clubConElMejorJugador(){
+        Optional<Player> result = CsvUtilFile.getPlayers().stream()
+                .reduce(((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2));
+        String club = result.get().getClub();
+        System.out.println("Club con el mejor jugador:");
+        System.out.println(club);
     }
 
     @Test
     void ElMejorJugador(){
+        Optional<Player> result = CsvUtilFile.getPlayers().stream()
+                .reduce(((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2));
+        String jugador = result.get().getName();
+        System.out.println("Mejor jugador:");
+        System.out.println(jugador);
     }
 
     @Test
     void mejorJugadorSegunNacionalidad(){
+        Map<String, Optional<Player>> result = CsvUtilFile.getPlayers().stream()
+                .collect(Collectors.groupingBy(
+                        Player::getNational,
+                        Collectors.reducing((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2)
+                ));
+        result.forEach((key, value) -> {
+            System.out.println(key + ": \n");
+            value.ifPresent(System.out::println);
+            System.out.println("\n");
+        });
     }
 
 
