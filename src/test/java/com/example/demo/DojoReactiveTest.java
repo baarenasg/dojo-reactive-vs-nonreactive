531ca9973606931794197b19698f7a6c92988dd9
baarenasg <baarenasg@unal.edu.co>
2023-07-03 19:39:52 -0500

    Update DojoReactiveTest.java

--- src/test/java/com/example/demo/DojoReactiveTest.java
+++ src/test/java/com/example/demo/DojoReactiveTest.java
@@ -53,32 +53,80 @@ public class DojoReactiveTest {
 
     @Test
     void mejorJugadorConNacionalidadFrancia(){
+        System.out.println("Mejor jugador Frances:");
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> fluxPlayers = Flux.fromIterable(list);
+        Mono<Player> result = fluxPlayers.filter(player -> player.getNational().equals("France"))
+                .reduce((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2);
+        result.subscribe(System.out::println);
     }
 
     @Test
     void clubsAgrupadosPorNacionalidad(){
+        List<Player> csv = CsvUtilFile.getPlayers();
+        Flux<Player> observable = Flux.fromIterable(csv);
+
+        observable
+                .groupBy(Player::getNational)
+                .flatMap(groupedFlux -> groupedFlux
+                        .collectList()
+                        .map(list->{
+                            List<String> clubs =new ArrayList<>();
+                            list.forEach(element -> clubs.add(element.getClub()));
+                            Map<String, List<String>> map =new HashMap<>();
+                            map.put(groupedFlux.key(),clubs);
+                            return   map;
+                        })).subscribe(map-> {
+                            map.forEach((key,value)->{
+                                System.out.println("\n");
+                                System.out.println(key + ": ");
+                                value.forEach(System.out::println);
+
+                            });
+                });
     }
 
     @Test
     void clubConElMejorJugador(){
+        System.out.println("Club con el mejor jugador:");
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> fluxPlayers = Flux.fromIterable(list);
+        Mono<Player> result = fluxPlayers.reduce((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2);
+        result.subscribe(player -> System.out.println(player.getClub()));
     }
 
-    @Test
-    void clubConElMejorJugador2() {
-    }
+
 
     @Test
     void ElMejorJugador() {
-    }
-
-    @Test
-    void ElMejorJugador2() {
+        System.out.println("El mejor jugador:");
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> fluxPlayers = Flux.fromIterable(list);
+        Mono<Player> result = fluxPlayers.reduce((p1, p2) -> p1.getWinners() > p2.getWinners()?p1:p2);
+        result.subscribe(player -> System.out.println(player.getName()));
     }
 
     @Test
     void mejorJugadorSegunNacionalidad(){
+        List<Player> csv = CsvUtilFile.getPlayers();
+        Flux<Player> observable = Flux.fromIterable(csv);
+
+        observable
+                .groupBy(Player::getNational)
+                .flatMap(groupedFlux -> groupedFlux
+                        .collectList()
+                        .map(list->{
+                            Player best = list.stream().reduce((p1,p2)->((p1.getWinners()/p1.getGames())>=(p2.getWinners()/p2.getGames()))?p1:p2).get();
+                            Map<String, Player> map =new HashMap<>();
+                            map.put(groupedFlux.key(),best);
+                            return   map;
+                        })).subscribe(map-> {
+                    map.forEach((key,value)->{
+                        System.out.println("\n");
+                        System.out.println(key + ": ");
+                        System.out.println(value);
+
+                    });
+                });
     }
-
-
-
 }
